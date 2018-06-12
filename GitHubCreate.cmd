3dd7cf1a35d4c30cf407b4f1d1223b9db0949568
k-schneid <40184149+k-schneid@users.noreply.github.com>
2018-06-12 10:58:15 -0700

    Initial commit

---
+++ GitHubCreate.cmd
@@ -0,0 +1,6 @@
+echo "# DSA_FromTest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/k-schneid/DSA_FromTest.git
+git push -u origin master
