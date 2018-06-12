f543608b1917177cee15ac4a8da525f27ef662ac
k-schneid <40184149+k-schneid@users.noreply.github.com>
2018-06-12 11:53:53 -0700

    Delete GitHubCreate.cmd

--- GitHubCreate.cmd
+++
@@ -1,6 +0,0 @@
-echo "# DSA_FromTest" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/k-schneid/DSA_FromTest.git
-git push -u origin master
